947ca8334a9f573dfc9e35e404d8289e762525f3
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-10-27 22:39:49 +0300

    uPDATING REDIS CACHE

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import cookieParse from 'cookie-parser';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParse());
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  expressApp.get('/', (req: Request, res: Response) => {
+    res.send('/Attendance API is running!');
+  });
   await app.listen(process.env.PORT ?? 9800);
 }
 await bootstrap();
