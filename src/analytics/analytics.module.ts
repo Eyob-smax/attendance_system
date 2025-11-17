6716350f7f899f73afc1feadeefbc9d32f8930b8
Eyob Simachew <eyob.ugr-1326-16@aau.edu.et>
2025-11-17 19:19:06 +0300

    adding a migration functionality for students

--- src/analytics/analytics.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AnalyticsResolver } from './analytics.resolver.js';
-import { AnalyticsService } from './analytics.service.js';
-
-@Module({
-  providers: [AnalyticsResolver, AnalyticsService],
-})
-export class AnalyticsModule {}
--- src/analytics/analytics.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AnalyticsService {
-  findAll() {
-    return ['pet1', 'pet2', 'pet3'];
-  }
-}
